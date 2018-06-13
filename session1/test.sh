56af83a58bda0c5578a7a68b782c4638bacee6d9
Dark96 <roysaurav90@gmail.com>
2018-06-13 18:32:31 +0530

    Update test.sh

--- session1/test.sh
+++ session1/test.sh
@@ -9,9 +9,9 @@ export THEANO_FLAGS=device=cuda0,floatX=float32
 
 python ./translate.py -n -p 20 \
 	$HOME/models/model_session1.npz  \
-	$HOME/data/europarl-v7.fr-en.en.tok.pkl \
-	$HOME/data/europarl-v7.fr-en.fr.tok.pkl \
-	$HOME/data/newstest2011.en.tok \
+	$HOME/codes/dl4mt-tutorial/data/europarl-v7.fr-en.en.tok.pkl \
+	$HOME/codes/dl4mt-tutorial/data/europarl-v7.fr-en.fr.tok.pkl \
+	$HOME/codes/dl4mt-tutorial/data/newstest2011.en.tok \
 	./newstest2011.trans.fr.tok
 
 
