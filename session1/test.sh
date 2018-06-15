970b77df22a67c6c06a6f054854d59f39219bf8c
Dark96 <roysaurav90@gmail.com>
2018-06-15 16:44:15 +0530

    Update test.sh

--- session1/test.sh
+++ session1/test.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=24
-#PBS -l walltime=4:00:00
-#PBS -N session1_default
-#PBS -A course
-#PBS -q ShortQ
 
-export THEANO_FLAGS=device=cuda0,floatX=float32
+
+export THEANO_FLAGS=device=cpu,floatX=float32
 
 python ./translate.py -n -p 20 \
 	$HOME/models/model_session1.npz  \
